4cb7c16eb9d4962919dc60a3b9ffdd9b50668251
Frank Tang <ftang@chromium.org>
2025-10-27 15:36:44 -0700

    ICU-23238 Propagate memory error out

--- icu4c/source/i18n/inputext.cpp
+++ icu4c/source/i18n/inputext.cpp
@@ -54,8 +54,9 @@ void InputText::setText(const char *in, int32_t len)
     fRawLength = len == -1 ? static_cast<int32_t>(uprv_strlen(in)) : len;
 }
 
-void InputText::setDeclaredEncoding(const char* encoding, int32_t len)
+void InputText::setDeclaredEncoding(const char* encoding, int32_t len, UErrorCode &status)
 {
+    if (U_FAILURE(status)) return;
     if(encoding) {
         if (len == -1) {
             len = static_cast<int32_t>(uprv_strlen(encoding));
@@ -64,6 +65,7 @@ void InputText::setDeclaredEncoding(const char* encoding, int32_t len)
         len += 1;     // to make place for the \0 at the end.
         char* newEncoding = NEW_ARRAY(char, len);
         if (newEncoding == nullptr) {
+            status = U_MEMORY_ALLOCATION_ERROR;
             return;
         }
         uprv_free(fDeclaredEncoding);
